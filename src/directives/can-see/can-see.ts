fb137ca29f1ec844b3c9f2c4ceeecc16f3379775
vlondonoo1 <veronica.londono@yuxiglobal.com>
2018-07-09 10:08:37 -0500

    directive with input

--- src/directives/can-see/can-see.ts
+++ src/directives/can-see/can-see.ts
@@ -1,4 +1,4 @@
-import { Directive, Input,OnDestroy, HostBinding,OnInit, ElementRef, AfterViewInit, Renderer2, ViewContainerRef } from '@angular/core';
+import { Directive, Input,OnDestroy, HostBinding,OnInit, ElementRef, AfterViewInit, Renderer2, ViewContainerRef,TemplateRef } from '@angular/core';
 import { CommonproviderProvider } from './../../providers/commonprovider/commonprovider';
 import { AuthProvider } from './../../providers/auth/auth';
 
@@ -7,11 +7,16 @@ import { AuthProvider } from './../../providers/auth/auth';
 })
 export class CanSeeDirective implements OnInit, AfterViewInit, OnDestroy{
  
+ @Input('can-see') set component(val) {
+   
+   this.show(val);
  
+   };
 
-  constructor(public element: ElementRef, public renderer: Renderer2,private viewContainer: ViewContainerRef,private commonProvider: CommonproviderProvider, private authProvider: AuthProvider) {
+  constructor(public element: ElementRef, public renderer: Renderer2,private viewContainer: ViewContainerRef,private commonProvider: CommonproviderProvider, private authProvider: AuthProvider, private templateRef: TemplateRef<any>) {
     console.log('Hello CanSeeDirective Directive');
      // element.nativeElement.style.color = 'red'
+     
   }
   //   avoid(componente){    
   //   return this.commonProvider.avoidcomponents(componente)
@@ -21,6 +26,17 @@ export class CanSeeDirective implements OnInit, AfterViewInit, OnDestroy{
     ngAfterViewInit(){
       console.log('verito',this.element.nativeElement)
     }
+     
+       show(component_){
+        console.log(component_,"component____");
+        
+         if(!this.commonProvider.permitcomp(component_ ,this.authProvider.kindofuser())) {
+          this.viewContainer.clear();
+        } else {
+          this.viewContainer.createEmbeddedView(this.templateRef);
+
+        }
+      }
 
 
     ngOnInit(){
@@ -32,19 +48,20 @@ export class CanSeeDirective implements OnInit, AfterViewInit, OnDestroy{
       this.commonProvider.checkActions()
 
 
-
-
-      if (!this.commonProvider.permitcomp('other',user)){
-        //mode==val
-        this.renderer.addClass(this.element.nativeElement, 'showw')
-      } else{
-        this.element.nativeElement.style.color = 'red'
-        this.renderer.removeClass(this.element.nativeElement, 'showw'); 
-      }
+    
 
 
 
-      
+
+
+      // if (!this.commonProvider.permitcomp('other',user)){
+      //   //mode==val
+      //   this.renderer.addClass(this.element.nativeElement, 'showw')
+      // } else{
+      //   this.element.nativeElement.style.color = 'red'
+      //   this.renderer.removeClass(this.element.nativeElement, 'showw'); 
+      // }
+
 
 
 
